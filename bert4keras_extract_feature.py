51f821ea1d41b81f4978ad0a1d97da02011631e7
Aiwiscal <1621936636@qq.com>
2020-03-27 00:13:19 +0800

    add zhushi.

--- bert4keras_extract_feature.py
+++ bert4keras_extract_feature.py
@@ -5,19 +5,25 @@ from bert4keras.models import build_transformer_model
 from bert4keras.tokenizers import Tokenizer
 import numpy as np
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # 屏蔽GPU
 
+# config_path: albert模型结构描述文件路径
 config_path = 'model/albert_tiny_zh_google/albert_config_tiny_g.json'
+# checkpoint_path: TensorFlow checkpoint文件路径
 checkpoint_path = 'model/albert_tiny_zh_google/albert_model.ckpt'
+# dict_path: albert词汇表路径
 dict_path = 'model/albert_tiny_zh_google/vocab.txt'
 
-tokenizer = Tokenizer(dict_path, do_lower_case=True)
+# 载入预训练ALBERT模型
 model = build_transformer_model(config_path, checkpoint_path, model='albert',
-                                with_pool=True)  # 建立模型，加载权重
+                                with_pool=True)
 
-token_ids, segment_ids = tokenizer.encode(u'你好世界')
-print(token_ids, segment_ids)
+tokenizer = Tokenizer(dict_path, do_lower_case=True)
+token_ids, segment_ids = tokenizer.encode(u'你好 世界')
+print("Token ID: " + str(token_ids))
+print("Segment ID:" + str(segment_ids))
+
 
 print('\n ===== predicting =====\n')
 vec1 = model.predict([np.array([token_ids]), np.array([segment_ids])])
-print(vec1)
+print(vec1.shape)
